4f34584dacbede4a685fc10e993ae83f103657be
NicKyeaa <tin.jurcevic@gmail.com>
2022-10-25 23:05:02 +0200

    Added card style and test content

---
+++ src/components/Equipment/EquipmentEditCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import 'antd/dist/antd.min.css';
+import {
+    Card
+} from 'ant';
+
+export const EquipmentEditCard = () => { 
+    <>
+        <Card
+            title="Edit Equipment"
+            style={{
+                width: '100%',
+                height: '100%'
+            }}
+        >
+            <p>Test1</p>
+            <p>Test2</p>
+            <p>Test3</p>
+        </Card>
+    </>
+};
